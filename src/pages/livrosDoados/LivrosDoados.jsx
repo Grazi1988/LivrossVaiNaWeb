9780761e705808adaf4df4634655b0626d231953
Grazi1988 <grazi_510@hotmail.com>
2025-04-03 14:35:10 -0300

    atualização

--- src/pages/livrosDoados/LivrosDoados.jsx
+++ src/pages/livrosDoados/LivrosDoados.jsx
@@ -1,36 +1,35 @@
 import LivroProtagonista from "../../assets/doado.png"
 import s from "./livrosDoados.module.scss"
 import { useState, useEffect } from "react"
-import axios from 'axios'
+import axios from "axios"
 
 function LivrosDoados() {
+  const [livros, setLivros] = useState([])
 
-  const [livros,setLivros] = useState ([])
-  
-  const getLivros = async()=>{
-    const resposta = await axios.get("https://desafio-livros-api.onrender.com/livros")
-    setLivros(resposta.data.livros)
-    
-  } 
-  
-  useEffect(()=>{
-      getLivros()
-  },[])
+  const getLivros = async () => {
+    const resposta = await axios.get(
+      "https://desafio-livros-api.onrender.com/livros"
+    )
+    setLivros(resposta.data)
+  }
 
+  useEffect(() => {
+    getLivros()
+  }, [])
 
   return (
+
     <section className={s.livrosDoadosSection}>
       <h2>Livros Doados</h2>
-
-      <section className={s.doandoLivros}>
-        <section>
-          <img src={LivroProtagonista} alt="Imagem de um livro com a capa vermelha"/>
-          <div>
-            <h3>O protagonista</h3>
-            <p>Susanne Andrade</p>
-            <p>Ficção</p>
-          </div>
-        </section>
+      <section className={s.containerCards}>
+        {livros.map((item) => (
+          <section>
+            <div>
+              <h3>{item.titulo}</h3>
+              <img src={item.image_url} alt={item.titulo} />
+            </div>
+          </section>
+        ))}
       </section>
     </section>
   )
